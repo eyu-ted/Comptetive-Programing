36dfe982597098b6e369d6a627e687dc7115bac3
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2024-02-26 15:35:00 +0300

    Add solution for Fibonacci Number

--- leetcode-solution/leetcode/fibonacci-number.py
+++ leetcode-solution/leetcode/fibonacci-number.py
@@ -2,10 +2,8 @@ class Solution:
     def fib(self, n: int) -> int:
 
 
-        if n==0:
-            return 0
-        elif n==1 or n==2:
-            return 1
+        if n<2:
+            return n
         else:
             
             return self.fib(n-1)+self.fib(n-2)
