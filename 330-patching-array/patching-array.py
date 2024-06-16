f426c5ea95e5092316f9aded3e66ea4b2520bd1f
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2024-06-16 20:01:22 +0300

    Time: 49 ms (92.66%) | Memory: 16.6 MB (69.93%) - LeetSync

---
+++ 330-patching-array/patching-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        i=0
+        c=1
+        ans=0
+        while c <=n:
+            if i<len(nums) and nums[i]<=c:
+                c+=nums[i]
+                i+=1
+                
+            else:
+                ans+=1
+                c=c*2 
+            
+        return ans
+        
+
+
+
+
+        
+        
