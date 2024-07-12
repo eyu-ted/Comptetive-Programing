4faae9f3d731448357c00d995f513bb624d34645
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2024-07-13 00:54:15 +0300

    Time: 177 ms (25.00%) | Memory: 17.9 MB (32.69%) - LeetSync

---
+++ 1139-previous-permutation-with-one-swap/previous-permutation-with-one-swap.py
@@ -0,0 +1,15 @@
+class Solution:
+    def prevPermOpt1(self, arr: List[int]) -> List[int]:
+        for i in range(len(arr)-1,0,-1):
+            if arr[i]<arr[i-1]:
+                ind=i-1
+                break
+        else:
+            return arr
+        m=ind+1
+        for i in range(ind+1,len(arr)):
+            if arr[m]<arr[i] and arr[ind]>arr[i]:
+                m=i
+        arr[m],arr[ind]=arr[ind],arr[m]
+        return arr
+        
