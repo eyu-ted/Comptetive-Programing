a90dbbcd3ddc64e1d40e14093d6677f53c77e97c
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2024-08-12 21:36:11 +0300

    Time: 97 ms (85.21%) | Memory: 17.3 MB (84.62%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,23 +4,13 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
 
-        row =set()
-        col = set()
-
+        posZero=[]
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    row.add(i)
-                    col.add(j)
-        # print(sett)
-        for i in range(len(matrix)):
-
-            for j in range(len(matrix[0])):
-                if i in row:
-                    matrix[i][j] =0
-
-
-                if j in col:
-                    matrix[i][j] =0
-    
-        
+                if matrix[i][j]==0:
+                    posZero.append([i, j])
+        for i, j in posZero:     
+            for row in range(len(matrix)):
+                matrix[row][j]=0
+            for col in range(len(matrix[0])):
+                matrix[i][col]=0
