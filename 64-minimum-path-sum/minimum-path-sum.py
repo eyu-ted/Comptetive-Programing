f2f686e2272e2b062099b457c5a115e2b3173a1a
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2024-08-23 11:50:22 +0300

    Time: 170 ms (5.07%) | Memory: 18.2 MB (78.43%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,28 +1,35 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         
-        dp ={}
+        
+        matrix= []
+
+        for _ in range(len(grid)):
+            matrix.append([float("inf")]*len(grid[0]))
+
+
         def inbound(x,y):
-            return x<len(grid) and x>-1 and y<len(grid[0]) and y>-1
-        direction =[(0,1),(1,0)]
-        def dfs(i,j):
-            if i == len(grid)-1 and j == len(grid[0])-1:
-                return grid[i][j]
-            if (i,j) in dp:
-                return dp[(i,j)]
-
-            minn = float("inf")
-
-            for dr,dc in direction:
-                if inbound(i+dr,j+dc):
-                    minn =min(minn, dfs(i+dr,j+dc))
-            
-            dp[(i,j)] = grid[i][j] + minn
-            
-            return dp[(i,j)]
+            return x>-1 and x<len(grid) and y>-1 and y<len(grid[0])
 
 
-        return dfs(0,0)
-            
+        direction = [(1,0),(0,1)]
+
+        for i in range(len(grid)-1,-1,-1):
+            for j in range(len(grid[0])-1,-1,-1):
+                
+                minn = float("inf")
+                for dr ,dc in direction:
+                    if inbound(i+dr,j+dc) :
+                        minn = min(minn,grid[i+dr][j+dc])
+                if minn ==float("inf"):
+                    minn =0
+                grid[i][j] += minn
+
+        
+
+        return grid[0][0]
+
+
+
 
             
