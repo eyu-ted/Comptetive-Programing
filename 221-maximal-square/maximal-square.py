9866def9c4ce28c9aef62c91296d05a362793c50
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2024-09-01 23:22:02 +0300

    Time: 772 ms (10.51%) | Memory: 19.9 MB (25.63%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,30 +1,30 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-
-
         def inbound(x,y):
-            return x>-1 and x<len(matrix) and y>-1 and y<len(matrix[0])
-
-        maxx= 0
-        
+            return x < len(matrix) and x>-1 and y<len(matrix[0]) and y>-1
+        direction = [(1,0),(0,1),(1,1)]
         for i in range(len(matrix)-1,-1,-1):
             for j in range(len(matrix[0])-1,-1,-1):
-                matrix[i][j]=int(matrix[i][j])
-                dia=right=bot=0
-                if inbound(i+1,j+1):
-                    dia += int(matrix[i+1][j+1])
+                matrix[i][j] = int(matrix[i][j])
+                minn = float("inf")
+                for dr, dc in direction:
+                    if inbound(i+dr,j+dc):
+                        minn = min(minn ,matrix[i+dr][j+dc])
                     
-                if inbound(i,j+1):
-                    right += int(matrix[i][j+1])
-                if inbound(i+1,j):
-                    bot += int(matrix[i+1][j])
-                
-                if int(matrix[i][j]):
-                    matrix[i][j] += min(bot,min(right,dia)) 
-                    maxx= max(maxx,matrix[i][j])
+                    else:
+                        minn = 0
+                        break
+                if matrix[i][j]:
+                    matrix[i][j] += minn
         
-    
-        return maxx*maxx
-                
+
+        ans = float("-inf")
+        print(matrix)
+        for nums in matrix:
+            nums.append(ans)
+            ans = max(nums)
+        return ans**2
 
 
+
+        
