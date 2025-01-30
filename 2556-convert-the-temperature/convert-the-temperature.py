3e540153d2e44ee1c11f3b614b39ee978666d563
eyu-ted <127511748+eyu-ted@users.noreply.github.com>
2025-01-30 23:00:57 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.36%) - LeetSync

--- 2556-convert-the-temperature/convert-the-temperature.py
+++ 2556-convert-the-temperature/convert-the-temperature.py
@@ -1,3 +1,3 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
-        return [celsius + 273.15, celsius * 1.8 + 32.0]
+        return [celsius+273.15,(celsius*1.80)+32]
