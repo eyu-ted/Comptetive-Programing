26171feda8a438f3fdb307d5cc3b9d708998b1b2
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-02-25 01:01:34 +0300

    Time: 287 ms (35.95%) | Memory: 18.2 MB (58.24%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -2,7 +2,7 @@ class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
 
         power = []
-        dic = {}
+        lis = []
 
         for x in range(lo, hi+1):
             count = 0
@@ -14,10 +14,9 @@ class Solution:
                     num = 3 * num + 1
                 count += 1
             
-            dic[x] = count
-        lis = []
-        for key,val in dic.items():
-            lis.append([val,key])
+            lis.append([count,x])
+
+            
         
         lis.sort()
 
