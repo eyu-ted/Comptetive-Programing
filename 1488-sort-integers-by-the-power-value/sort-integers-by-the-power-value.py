f85c9588db042895b2649a6c50ee924d9bec340a
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-02-25 01:10:19 +0300

    Time: 87 ms (88.01%) | Memory: 33.7 MB (24.08%) - LeetSync

--- 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
+++ 1488-sort-integers-by-the-power-value/sort-integers-by-the-power-value.py
@@ -1,19 +1,23 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
-
-        power = []
+        dic = {}
+        dic[1]  = 0
+        def dp(num):
+            if num in dic:
+                return dic[num]
+            
+            if num %2 == 0:
+                val = 1 + dp(num//2)
+            else:
+                val = 1 + dp((num * 3) + 1)
+            
+            dic[num] = val
+            return dic[num]
+            
         lis = []
 
         for x in range(lo, hi+1):
-            count = 0
-            num = x
-            while num > 1:
-                if num % 2 == 0:
-                    num //= 2
-                else:
-                    num = 3 * num + 1
-                count += 1
-            
+            count = dp(x)
             lis.append([count,x])
 
             
