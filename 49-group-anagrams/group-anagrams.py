76f395ba70e136c222559f6ef4da76a3d65b1ae9
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-07-13 13:46:26 +0300

    Time: 10 ms (87.27%) | Memory: 20.9 MB (56.26%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        soln_dic = defaultdict(list)
+        for word in strs:
+            soln_dic["".join(sorted(list(word)))].append(word)
+        
+        soln = []
+        for _, value in soln_dic.items():
+            soln.append(value)
+
+        return soln
+
+        # nat = [n,a,t] = [a,n,t] = [ant] = nat
+
+        # tan = [t,a,n] = [a,n,t] = [ant] = nat , tan
