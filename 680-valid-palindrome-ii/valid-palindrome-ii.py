493078c01cdb7f0bfe262120b4470f8931a5b861
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-08-04 23:29:36 +0300

    Time: 30 ms (92.96%) | Memory: 18 MB (55.45%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        count = 0
+        l=0
+        r= len(s)-1
+
+        while l<r:
+            if s[l] != s[r]:
+                
+                skip_left = s[l+1:r+1]
+                skip_right = s[l:r]
+
+                return skip_left == skip_left[::-1] or skip_right == skip_right[::-1]
+            l+=1
+            r-=1
+        return True
+
+
+
+        
