fbb1496902f810e95d18d1cdf074d1f50e3adedb
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-10-20 21:43:30 +0300

    Time: 3 ms (93.23%) | Memory: 19.2 MB (65.34%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,18 +1,31 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        mapper = defaultdict(int)
+        for i in range(len(graph)):
+            mapper[i] = graph[i]
 
-        answer = []
-        def dfs(node ):
+        result = []
+        def dfs(node,path):
+
             if node == len(graph)-1:
-                answer.append(path[:])
+                
+                result.append(path.copy())
                 return
-            
-            for nei in graph[node]:
-                path.append(nei)
-                dfs(nei)
+
+
+
+            for neigh in mapper[node]:
+                path.append(neigh)
+                dfs(neigh,path)
                 path.pop()
+        
+        dfs(0,[0])
 
-        path = [0]      
-        dfs(0)
-        return [list(x) for x in answer]
+        return result
+                
 
+
+            
+
+
+        
