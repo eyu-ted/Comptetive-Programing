cd1ec4a32bd812843878c77cb0c7f2ace408c42f
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-11-02 11:59:55 +0300

    Time: 32 ms (26.32%) | Memory: 30.6 MB (11.53%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -2,21 +2,21 @@ class UndergroundSystem:
 
     def __init__(self):
         self.dic = {}
-        self.summ = defaultdict(int)
-        self.count = defaultdict(int)
+        self.summ = {}
         
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.dic[id] = (stationName,t)       
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-
-        self.summ[(self.dic[id][0], stationName)] += t- self.dic[id][1]
-        self.count[(self.dic[id][0], stationName)] += 1
-
+        if (self.dic[id][0], stationName) not in self.summ:
+            self.summ[(self.dic[id][0], stationName)] = [t- self.dic[id][1],1]
+        else:
+            self.summ[(self.dic[id][0], stationName)][0] += t- self.dic[id][1]
+            self.summ[(self.dic[id][0], stationName)][1] += 1
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        return self.summ[(startStation, endStation)] /self.count[(startStation, endStation)] 
+        return self.summ[(startStation, endStation)][0] /self.summ[(startStation, endStation)][1] 
         
 
 
