626398c8a1784366babdad7a25843e4876429668
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-11-06 11:11:50 +0300

    Time: 4 ms (4.44%) | Memory: 17.8 MB (65.05%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,18 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
-        sett = set()
+        sett = []
 
         def dfs(i, path):
-            if i>len(nums):
-                return
-            if tuple(sorted(path)) not in sett:
-                sett.add(tuple(sorted(path.copy())))
+            
+            sett.append(path.copy())
             
             for j in range(i,len(nums)):
                 path.append(nums[j])
                 dfs(j+1, path)
                 path.pop()
         dfs(0,[])
-        return list(sett)
+        return sett
         
