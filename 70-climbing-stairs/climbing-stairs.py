2ead6aafdf5be994046ffebe78c473126275dadc
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-11-08 23:31:55 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.03%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,12 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @cache
+        memo ={}
         def dfs(n):
             if n<0:
                 return 0
             if n <=1:
                 return 1
-            
-            return dfs(n-1) + dfs(n-2)
+            if n not in memo:
+                memo[ n] = dfs(n-1) + dfs(n-2)
+            return memo[n]
         
         return dfs(n)
