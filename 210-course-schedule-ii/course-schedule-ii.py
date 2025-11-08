afee710c28c8b1bfa3b6e610e0c70bacd29189f4
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-11-08 18:21:26 +0300

    Time: 3 ms (76.97%) | Memory: 19 MB (71.16%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        from collections import defaultdict, deque
+        dic = defaultdict(list)
+        indgree = defaultdict(int)
+        
+        for a,b in prerequisites:
+            dic[b].append(a)
+            indgree[a]+=1
+        
+        q = deque()
+        visited = set()
+        result = []
+        for i in range(numCourses):
+            if i not in indgree:
+                q.append(i)
+                visited.add(i)
+                result.append(i)
+
+        
+        while q:
+            node = q.popleft()
+
+            for neigh in dic[node]:
+                indgree[neigh] -=1
+                if indgree[neigh] == 0 and neigh not in visited:
+                    q.append(neigh)
+                    result.append(neigh)
+                    visited.add(neigh)
+        
+
+                
+
+        return result if len(result) == numCourses else []
