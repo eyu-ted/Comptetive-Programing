c103a7846a21f3ff6b33ef413cf35b1da95d1683
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-11-10 18:38:58 +0300

    Time: 167 ms (68.48%) | Memory: 42.7 MB (54.35%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -1,11 +1,11 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
-        
+        from bisect import bisect_left,bisect_right
         result  = []
         potions.sort()
         for spell in spells:
             num = math.ceil(success/spell)
-            indx = bisect_left(potions,num)
+            indx = bisect.bisect_left(potions,num)
      
             result.append(len(potions)- indx)
         return result
