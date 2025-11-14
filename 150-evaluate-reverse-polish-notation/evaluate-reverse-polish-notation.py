167dc807713ab8b3eadd765b8c1095c153d38e16
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-11-14 15:19:55 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (51.55%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -2,26 +2,31 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         
         stack = []
+        result = 0
+        opp = {"+","-","*","/"}
+        for ch in tokens:
 
-        for tok in tokens:
+            if ch in opp:
+                num1 = stack.pop()
+                num2 = stack.pop()
+                if ch == "+":
+                    temp = (num1+num2)
+                elif ch =="*":
+                    temp = (num1*num2)
+                elif ch == "-":
+                    temp = (num2-num1)
+                else:
+                    if num2/num1 < 0:
+                        temp = -1*int(abs(num2/num1))
+                    else:
+                        temp = num2//num1
 
-            if tok == "+":
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(num1+num2)
-            elif tok == "-":
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(num2-num1)
-            elif tok == "*":
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(num1*num2)
-            elif tok == "/":
-                num1 = stack.pop()
-                num2 = stack.pop()
-                stack.append(int(num2/num1))
+
+                    
+                stack.append(temp)
             else:
-                stack.append(int(tok))
-        return stack[-1]
+                stack.append(int(ch))
+        
+            
 
+        return stack[0]
