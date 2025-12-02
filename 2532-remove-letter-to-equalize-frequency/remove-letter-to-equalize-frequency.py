63632063ba14f213c191387b681c5d0d0cbe61ac
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-12-02 15:34:13 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.66%) - LeetSync

--- 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/remove-letter-to-equalize-frequency.py
@@ -1,11 +1,12 @@
 class Solution:
     def equalFrequency(self, word: str) -> bool:
-        
+        if word.count(word[0]) == len(word):
+            return True
         freq_count = Counter(Counter(word).values())
         
         if len(freq_count) == 1: 
             # check if either every char occured only once 1 in freq_count OR  if all the characters are similar or duplicates 1 in _freq_count.values()
-            return 1 in freq_count or 1 in freq_count.values()
+            return 1 in freq_count 
         
         elif len(freq_count) == 2: 
 
