2102fdfb7b7fcddae8860d85ec4cee96c674c856
Eyoel Tedla Bekele <127511748+eyu-ted@users.noreply.github.com>
2025-12-08 20:57:42 +0300

    Time: 23 ms (26.46%) | Memory: 21 MB (15.63%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,21 +1,17 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        if n == 1 and trust == []:
-            return 1
-        trusted_person = []
-        dic = defaultdict(set)
-
-        for trustee , trusted in trust:
-            trusted_person.append(trusted)
-            dic[trustee].add(trusted)
         
-        for person in trusted_person:
-            if person not in dic:
-                flag = True
-                for key in range(1,n+1):
-                    if key != person and person not in dic[key]:
-                        flag = False
-                        break
-                if flag:
-                    return person
+        trust_dic = defaultdict(list)
+        trusted_by_dic = defaultdict(list)
+
+
+        for a,b in trust:
+            trust_dic[a].append(b)
+            trusted_by_dic[b].append(a)
+        
+        for num in range(1,n+1):
+            
+            if len(trusted_by_dic[num]) == n-1 and num not in trust_dic:
+                return num
+        
         return -1
